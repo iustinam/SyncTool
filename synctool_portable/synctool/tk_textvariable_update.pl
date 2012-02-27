1d7a9745ee1f9a17c458755e552e5f6c652b2248
Melinte Iustina <iustina.m.90@gmail.com>
2012-02-27 17:10:16 +0200

    gui restore jobs conf_edit global conf restore

---
+++ synctool_portable/synctool/tk_textvariable_update.pl
@@ -0,0 +1,39 @@
+use warnings;
+use strict;
+use threads;
+use threads::shared;
+
+use Tk;
+
+my $mw = new MainWindow;    
+my $h;
+$h=&share({});
+ 
+ share $h->{time};
+ $h->{time} = time();
+ 
+ my  $t;
+ share $t;
+ $t=time();
+ 
+#my $label = $mw->Entry(-textvariable => \$h->{time})->pack();
+my $label = $mw->Entry(-textvariable => \$t)->pack();
+
+threads->new(sub{   print "here";
+    sleep(5);
+    {
+        lock($t);
+        $t = time();
+    }
+#    sleep(5);
+#    $h->{time} = time();
+#    sleep(5);
+#    $h->{time} = time();
+})->detach();
+    
+#$mw->repeat(1000, sub{ $h->{time} = time(); } );
+MainLoop;
+
+ 
+
+
